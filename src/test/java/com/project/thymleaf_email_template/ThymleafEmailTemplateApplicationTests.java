9e69358e138003f64c996eb323a92afda1d013eb
adamchok <adam.c11304@gmail.com>
2024-08-12 15:12:57 +0800

    Initial commit

---
+++ src/test/java/com/project/thymleaf_email_template/ThymleafEmailTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.thymleaf_email_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymleafEmailTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
